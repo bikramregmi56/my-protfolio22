8ce25f4c3f1063590fde67ded1e6778ba62dd376
Bikram Regmi <bikramregmi56@gmail.com>
2026-02-28 11:00:34 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll effect
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
